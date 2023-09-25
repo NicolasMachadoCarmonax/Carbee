dcef8db3b563001379b8e1b3ead74049acd9813e
Isabella Hachmeister <isabellahachmeister@MacBook-Air-de-Isabella.local>
2023-09-25 16:25:05 -0600

    feat: production ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client"
 import { useRouter } from "next/navigation";
 import styles from './page.module.css';
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-  router.replace('/dashboard');
+
+  useEffect(() => {
+    router.replace('/dashboard');
+  }, []);
 
   return (
     <div className={styles.home}>
