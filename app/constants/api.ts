273892f16f7f35560e8e568bf0e3989fa6ad6836
Isabella Hachmeister <isabellahachmeister@MacBook-Air-de-Isabella.local>
2023-09-25 17:19:39 -0600

    fix: api in production does not work

--- app/constants/api.ts
+++ app/constants/api.ts
@@ -2,7 +2,7 @@
 export const awsRegion: string = process.env.AWS_REGION || '';
 export const awsPoolId: string = process.env.AWS_POOL_ID || '';
 export const awsClientId: string = process.env.AWS_CLIENT_ID || '';
-export const appURL: string = process.env.APP_URL || "http://localhost:3000";
+export const appURL: string = "https://carbee.store";
 export const authURL: string = `${appURL}/api/auth`;
 export const logoutURL: string = `${appURL}/api/logout`;
 
